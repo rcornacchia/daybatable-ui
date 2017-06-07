32c5df3a325ee5063b04525adcf0786123ebaec0
rcornacchia <rlc2160@columbia.edu>
2017-06-06 23:40:04 -0400

    added action type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+const LOAD_ARGUMENTS = 'LOAD_ARGUMENTS';
+
 export const LoadArguments = (args) => {
     return {
-        type: 'LOAD_ARGUMENTS',
+        type: LOAD_ARGUMENTS,
         args
     }
 }
