d3671e62d85eb7e830e10132a6c4067daca14acd
rcornacchia <rlc2160@columbia.edu>
2017-06-07 22:39:54 -0400

    fixed header to show username if user is logged in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+// import jwt from 'jsonwebtoken';
 import './Header.scss';
 
+function isAuthenticated() {
+  const token = localStorage.getItem('token');
+  // return jwt.check(token, (isValid, err) => {
+    if (token) return true;
+    return false;
+  // });
+}
+
 const Header = () => {
+  const token = localStorage.getItem('token');
+  const username = localStorage.getItem('username');
+
+  let navRight = <div>{username}</div>;
+  if (!token) {
+    navRight = (
+      <div>
+        <Link to='/login' className='link auth-link'>Login</Link>
+        <Link to='/register' className='link auth-link'>Register</Link>
+      </div>
+    );
+  }
+  
   return (
     <div className='header-container'>
-      <Link to='/' className='link'>Daybatable</Link>
-      <div className='auth-links'>
-        <Link to='/login' className='link auth-link'>Login</Link>
-        <Link to='register' className='link auth-link'>Register</Link>
+      <div className='navbar'>
+        <Link to='/' className='link'>Daybatable</Link>
+        <div className='navbar-right'>
+          {navRight}
+        </div>
       </div>
     </div>
   );
