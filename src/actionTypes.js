a4a1e67da62a58586e8cbfe6ac9b879f4b92dbac
rcornacchia <rlc2160@columbia.edu>
2017-07-11 11:20:18 -0400

    FEAT: can now get initial payload

---
+++ src/actionTypes.js
@@ -0,0 +1,3 @@
+export const INIT         = 'INIT';
+export const INIT_SUCCESS = 'INIT_SUCCESS';
+export const INIT_FAIL    = 'INIT_FAIL';
---
+++ src/sagas.js
@@ -0,0 +1,19 @@
+import { takeLatest, put, call } from 'redux-saga/effects';
+import * as actions from './actionTypes';
+import { init } from './api';
+
+const rootSaga = function* rootSaga() {
+  yield takeLatest(actions.INIT, initSaga);
+}
+
+function* initSaga() {
+  try {
+    const response = yield call(init);
+    console.log(response);
+    yield put({ type: actions.INIT_SUCCESS, response });
+  } catch (error) {
+    yield put({ type: actions.INIT_FAIL, error });
+  }
+}
+
+export default rootSaga;
