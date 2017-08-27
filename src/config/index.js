6e694494ddedecdbeb82aaf4ffad25627fa7d0cc
rcornacchia <rlc2160@columbia.edu>
2017-08-26 22:00:59 -0400

    updated config file

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import devConfig from './config.dev';
 import prodConfig from './config.prod';
 
 let config;
-(process.env.MODE === 'PROD') ? config = prodConfig
-                              : config = devConfig;
+(process.env.MODE === 'DEV') ? config = devConfig
+                             : config = prodConfig;
 
 export default devConfig;
