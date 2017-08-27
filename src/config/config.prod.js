e9895421b6f655765a0f016b00e5b0b5b7f26ed5
rcornacchia <rlc2160@columbia.edu>
2017-08-26 21:49:10 -0400

    updated config

---
+++ src/config/config.prod.js
@@ -0,0 +1,5 @@
+const prodConfig = {
+  server: 'http://http://ec2-13-59-9-213.us-east-2.compute.amazonaws.com/'
+}
+
+export default prodConfig
--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,8 @@
 import devConfig from './config.dev';
+import prodConfig from './config.prod';
+
+let config;
+(process.env.MODE === 'PROD') ? config = prodConfig
+                              : config = devConfig;
 
 export default devConfig;
