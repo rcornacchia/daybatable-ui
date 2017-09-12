7f8d4f1ee5591a082fd5119c7295a97c0093489b
rcornacchia <rlc2160@columbia.edu>
2017-09-12 07:27:46 -0400

    added google analytics and updated package.json

--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ import DevTools from './containers/DevTools';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-106316218-1', 'auto');
+ga('send', 'pageview');
+
+
 const store = configureStore(rootReducer);
 store.dispatch({ type: 'INIT' });
 
