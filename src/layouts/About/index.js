77c60d8d7572fd86e35a7796528b22b2fc90ddac
rcornacchia <rlc2160@columbia.edu>
2018-02-22 08:53:54 -0500

    updated About page

--- src/layouts/About/index.js
+++ src/layouts/About/index.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Card from '../../components/Card';
 
 const About = () => (
   <Container>
     <AboutContainer>
-      <Card>
         <AboutText>
-          <div className='section'>
+          <div>
             <div>This site hosts one debate everyday.</div>
             <div>The debate starts at noon Eastern Time, and ends 24 hours later.</div>
             <div>The upcoming debate with the most votes is selected as the next debate.</div>
             <br />
-            <div><strong>There are five ways to participate:</strong></div>
           </div>
-          <div className='section'>
+          <List>
+            <div><strong>There are five ways to participate:</strong></div>
             <ol>
               <li>Vote for a side</li>
               <li>Post an argument for a side</li>
@@ -22,9 +20,8 @@ const About = () => (
               <li>Suggest an upcoming debate topic</li>
               <li>Vote for an upcoming debate</li>
             </ol>
-          </div>
+          </List>
         </AboutText>
-      </Card>
     </AboutContainer>
   </Container>
 )
@@ -34,16 +31,26 @@ export default About;
 const AboutText = styled.div`
   margin: 0 auto;
   font-family: 'Courier';
-  font-size: 18px;
+  font-size: 20px;
+  color: white;
+`
+
+const List = styled.div`
+  width: 425px;
+  margin: 0 auto;
 `
 
 const AboutContainer = styled.div`
-  position: relative;
-  top: 20px;
+  width: 800px;
+  background-color: rgba(0,0,0, 0.15);
+  padding: 20px;
 `
 
 const Container = styled.div`
   height: calc(100vh - 61px);
   width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: linear-gradient(to right, #00A8E8, #F22B4A);
 `
